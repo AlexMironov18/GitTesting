8ec45466b7f52861892db39a5a7073404c44e0fa
Alex Mironov <nomanyash@gmail.com>
2020-09-16 10:49:21 +0400

    [DexSys2020-1] 2 commit in master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This should be in master branch");
+    }
+    public static void Master1() {
+        System.out.println("Added another commit into master");
     }
 }
 
